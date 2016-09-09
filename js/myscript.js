0f6d62a4372efe4edc7e5131c4de8023f5ea7948
Paliy Rostyslav <paliy1984@gmail.com>
2016-09-09 20:28:36 +0300

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+/* Coded by Paliy Rostyslav. e-mail: paliy1984@gmail.com. skype: ros.coprandos */
+
+/* Random BackGround */
+
+
+var imageArr = ["linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(/img/bg_01.jpg)", "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(/img/bg_02.jpg)", "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(/img/bg_03.jpg)"];
+
+var x = Math.floor(Math.random() * imageArr.length);
+
+var image = imageArr[x];
+
+$('body').css('background-image', image);
+
+
+
+
